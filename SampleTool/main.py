ceb9f5a5b744bf4c6a0f41818e42e3601d2fa214
DatDonathan <jojo08082003@gmail.com>
2023-01-15 18:43:33 +0100

    Render by notes

--- SampleTool/main.py
+++ SampleTool/main.py
@@ -6,6 +6,12 @@ import soundfile
 import numpy
 import pathlib
 
+class SampleToolVelocity:
+
+    def __init__(self, name, velocity) -> None:
+        self.name = name
+        self.velocity = velocity
+
 class SampleToolParams:
 
     def __init__(self):
@@ -15,12 +21,12 @@ class SampleToolParams:
         self.start_note=22
         self.end_note=108
         self.note_step=3
-        self.velocities=[127]
+        self.velocities=[SampleToolVelocity("FFF", 127)]
         self.vst_path=''
         self.preset_path=''
         self.save_preset_path=''
         self.dist_path='./'
-        self.filename_pattern='{name}_{velocity}_{note}_{step}.wav'
+        self.filename_pattern='{name}_{velocity}_{note}.wav'
         self.press_duration=20.0
         self.duration=22.0
         self.open_editor=False
@@ -64,25 +70,23 @@ def main():
     if config.save_preset_path != '':
         plugin.save_state(config.save_preset_path)
     #Open
-    #Process
+    #Velocities
     for velocity in config.velocities:
+        print("Processing velocity ", velocity.name, " (", velocity.velocity, ")")
         #Notes
         note = config.start_note
-        time = 1.0
         while note <= config.end_note:
-            print("Adding note ", note)
-            plugin.add_midi_note(note, velocity, time, config.press_duration)
+            print("Rendering note ", note)
+            graph= [
+                (plugin, [])
+            ]
+            plugin.add_midi_note(note, velocity.velocity, 0, config.press_duration)
+            engine.load_graph(graph)
+            engine.render(config.duration)
+
+            soundfile.write(folder + '/' + config.dist_path + '/' + velocity.name + '/' + config.filename_pattern.format(name=config.name, note=config.start_note, velocity=velocity.name, step=config.note_step), engine.get_audio().transpose(), config.sample_rate, subtype='PCM_24')
             note += config.note_step
-            time += config.duration
-        #Render
-        print("Rendering velocity ", velocity, " Duration: ", time/60.0 , " min")
-        graph= [
-            (plugin, [])
-        ]
-        engine.load_graph(graph)
-        engine.render(time)
-        print("Finished rendering velocity ", velocity)
-        soundfile.write(folder + '/' + config.dist_path + '/' + config.filename_pattern.format(name=config.name, note=config.start_note, velocity=velocity, step=config.note_step), engine.get_audio().transpose(), config.sample_rate, subtype='PCM_24')
        
 if __name__ == '__main__':
     main()
+
